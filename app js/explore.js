5ea44d3aee6af6de9b265f70d2346c2371804555
Mak-43 <letsmailmak@gmail.com>
2022-03-05 21:13:21 +0600

    cookie

---
+++ app js/explore.js
@@ -0,0 +1,7 @@
+const name=15;
+const student={ name:'jolil',friend:'borsha',job:'Business',
+makeMovie:function(name){
+    console.log(name)
+}}
+student.makeMovie('mission impossible')
+document.getElementById('name');
